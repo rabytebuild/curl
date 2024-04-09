3d309095fd8429f158683981445132c7b41078d2
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2024-04-09 04:12:37 +0100

    Update notebook.sh

--- notebook.sh
+++ notebook.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Update package lists
-sudo apt update
-
-# Install Python and pip
-sudo apt install -y python3 python3-pip
-
 # Install Jupyter Notebook
 python3 -m pip install notebook
 
 # Download and extract ngrok
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-
 tar -xvf ngrok-v3-stable-linux-amd64.tgz
 
+# Authenticate ngrok
 ./ngrok authtoken 2Hd7yeF4INCKbg2aP9rGMLnDqBX_5K7WhATjW8eUxS6UoHSRa
 
-./ngrok http 8888 & python3 -m notebook --allow-root
+# Start ngrok to expose Jupyter Notebook port
+./ngrok http 8888 &
+
+# Start Jupyter Notebook with root access
+python3 -m notebook --allow-root
