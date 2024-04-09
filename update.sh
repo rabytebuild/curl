116321786cc7f5f91775bb9628dd29287bc21eea
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2024-04-09 03:56:11 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 sudo apt update
 
 # Install default packages and tools
-sudo apt install -y ubuntu-desktop^
+# sudo apt install -y ubuntu-desktop^
 
 # Additional tools
 sudo apt install -y build-essential  # for compiling software
@@ -14,7 +14,7 @@ sudo apt install -y wget  # command line tool for downloading files from the web
 sudo apt install -y unzip  # utility for unpacking zip archives
 sudo apt install -y vim  # text editor
 sudo apt install -y nano  # text editor
-sudo apt install -y gnome-tweaks  # tool for customizing GNOME desktop environment
+# sudo apt install -y gnome-tweaks  # tool for customizing GNOME desktop environment
 sudo apt install -y htop  # interactive process viewer
 sudo apt install -y net-tools  # networking utilities
 sudo apt install -y openssh-server  # secure shell (SSH) server
